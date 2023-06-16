3dd42017660e5d11ee19376565ffaa018eccb42c
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-06-16 13:44:29 +0530

    handled errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,40 +7,52 @@ import Loader from './components/Loader';
 
 function App() {
 
-  const [movies, setMovies]= useState([])
-  const [loading,setLoading] = useState(false)
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  
 
-
-  const fetchMoviesHandler = async()=>{
-    try{
+  const fetchMoviesHandler = async () => {
+    try {
+      setError(false)
 
       setLoading(true)
-      const rawData = await fetch('https://swapi.dev/api/films/')
-
-      const data = await rawData.json();
+      const response = await fetch('https://swapi.dev/api/films/')
+      console.log(response)
+      if (!response.ok) {
+        setError(true)
+        throw new Error('Something went wrong ....Retrying')
+      }
+      const data = await response.json();
       setMovies(data.results);
-     
-    }catch (err){
+    
+    } catch (err) {
+      setLoading(false)
       console.log(err)
+     
+
     }
 
     setLoading(false)
-  } 
+  }
+
+  
 
   return (
-   
+
     <React.Fragment>
-     {loading?  <Loader />: <div>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-      </div>}
-    
+      <div>
+        <section>
+          <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        </section>
+        <section>
+          {error && <p className='err'>Something went wrong ....Retrying <span><button >cancel</button></span></p>}
+          {loading && <Loader />}
+          <MoviesList movies={movies} />
+        </section>
+      </div>
     </React.Fragment>
-    
+
   );
 }
 
